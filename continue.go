fc0efedcb90aeaced3f777ac9beabf5c2b8fa847
reski-id <pesan.reski@gmail.com>
2022-06-25 20:05:09 +0700

    case function()

--- continue.go
+++ continue.go
@@ -4,9 +4,14 @@ import "fmt"
 
 func main() {
 	for i := 0; i < 10; i++ {
-		if i % 2 == 0 {
+		if i%2 == 0 {
 			continue
 		}
 		fmt.Println("Perulangan ke", i)
 	}
 }
+
+/** case loop-continue[1]
+	1. Temukan Angka Ganjil
+	2. Buatlah array 1-9, Jika ada 4689 skip, print angka selain 4689
+**/
--- function.go
+++ function.go
@@ -1,15 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func sayHello() {
 	fmt.Println("Hello World")
-	fmt.Println("Hello World")
-	fmt.Println("Hello World")
+}
+
+//case_Function[1] -- adding two integer
+func Penjumlahan(number1 int32, number2 int32) int32 {
+	return number1 + number2
+}
+
+//case_Function[2] -- cek jika number ganjil return true
+func Is_Ganjil(number int) bool {
+	if number%2 == 1 {
+		return true
+	}
+	return false
 }
 
 func main() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 3; i++ {
 		sayHello()
 	}
+
+	fmt.Println(Penjumlahan(45, 23))
+	fmt.Println(Is_Ganjil(9))
 }
+
+/** case [Function]
+1. Function Penjumlahan
+2. Function Mencari Bilangan Ganjil
+3. Fibonaccy
+4. Prima
+**/
